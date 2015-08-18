078a2f3a27e801f1439969ccf82d01e21bbe9b77
Dan Weaver <danweaver@exosite.com>
2015-08-17 23:34:12 -0500

    rudimentary library

--- README.md
+++ README.md
@@ -2,4 +2,26 @@
 
 Javascript library for [Exosite](http://exosite.com)'s fleet management APIs, currently in beta.
 
+## Usage
 
+- add library and to your HTML. The library is dependent on jQuery and Auth0. Later it will not be. 
+
+```
+<head>
+    <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+    <script src="http://cdn.auth0.com/w2/auth0-4.js"></script>
+    <script src="https://github.com/dweaver/exosite-fleet/blob/master/src/Exosite.js"></script>
+</head>
+```
+
+- TODO
+
+
+
+## Build for distribution
+
+To build the build, do this:
+
+```
+$ npm build
+```
--- src/Exosite.js
+++ src/Exosite.js
@@ -4,12 +4,61 @@
  */
 class Exosite {
 
-  constructor(name) {
+  constructor(apiServer, userToken) {
+    this.API_SERVER = apiServer || 'https://fleet-prototype-api.herokuapp.com';
     this.name = name || 'Guest';
+    // TODO: drop $ dependency
+    $.ajaxSetup({
+      beforeSend: function(xhr) {
+        if (userToken) {
+          xhr.setRequestHeader('Authorization',
+                'Bearer ' + localStorage.getItem('userToken'));
+        }
+      }
+    });
   }
 
-  hello() {
-    return `Welcome, ${this.name}!`;
+  query(query, selection, options) {
+    var deferred = $.Deferred();
+     
+    devices = $.ajax(
+        API_SERVER + '/api/v1/Devices',
+        {
+          data: {
+            query: query,
+            select: selection
+          }
+        })
+      .done(function(data) {
+        deferred.resolve(data);
+      })
+      .fail(function(err) {
+        deferred.reject(err);
+      });
+    return deferred.promise();
+  }
+
+  rpc(auth, calls) {
+    var deferred = $.Deferred();
+    results = $.ajax(
+      API_SERVER + '/onep:v1/rpc/process',
+      {
+        type: 'POST',
+        processData: false,
+        contentType: 'application/json',
+        data: JSON.stringify({
+          auth: auth,
+          calls: calls
+        })
+      })
+    .done(function(response) {
+      deferred.resolve(response);
+    })
+    .fail(function(err) {
+      deferred.reject(err); 
+    });
+
+    return deferred.promise();
   }
 
 }
