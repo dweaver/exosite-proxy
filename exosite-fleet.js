9d9d44cf5cdfa42e605879a98b267b124f831b7d
Dan Weaver <danweaver@exosite.com>
2015-09-03 23:18:05 -0500

    0.1.5 rebuild

--- exosite-fleet.js
+++ exosite-fleet.js
@@ -1,4 +1,4 @@
-require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+require=(function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);throw new Error("Cannot find module '"+o+"'")}var f=n[o]={exports:{}};t[o][0].call(f.exports,function(e){var n=t[o][1][e];return s(n?n:e)},f,f.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 /**
  * Module dependencies.
  */
@@ -1329,7 +1329,9 @@ module.exports = function(arr, fn, initial){
   
   return curr;
 };
-},{}],4:[function(require,module,exports){
+},{}],"exosite-fleet":[function(require,module,exports){
+module.exports=require('FXviCq');
+},{}],"FXviCq":[function(require,module,exports){
 'use strict';
 
 Object.defineProperty(exports, '__esModule', {
@@ -1432,4 +1434,4 @@ var Exosite = (function () {
 exports['default'] = Exosite;
 module.exports = exports['default'];
 
-},{"../node_modules/superagent/lib/client.js":1}]},{},[4]);
+},{"../node_modules/superagent/lib/client.js":1}]},{},["FXviCq"])
